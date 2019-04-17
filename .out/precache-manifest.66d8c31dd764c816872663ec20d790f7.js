0ac636c0a1bca826ea3f8674f79c720f0efe194d
SonLV <apple@SonLV.local>
2019-04-17 16:05:32 +0700

    add build foder

---
+++ .out/precache-manifest.66d8c31dd764c816872663ec20d790f7.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "main.e5103a37c29d45e9901b.bundle.js"
+  },
+  {
+    "url": "runtime~main.e5103a37c29d45e9901b.bundle.js"
+  },
+  {
+    "url": "vendors~main.e5103a37c29d45e9901b.bundle.js"
+  },
+  {
+    "revision": "6f1991788bf7c39e98646ee9ed5df905",
+    "url": "iframe.html"
+  }
+];
